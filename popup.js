94cd05d1eee1248014423532663b0e5fdb37fbe6
YiChun Sung <ycsung.r@gmail.com>
2023-08-06 18:47:35 +0800

    feat: added content.js

--- popup.js
+++ popup.js
@@ -1,1 +1,47 @@
-console.log('This is a popup!');
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.action.setBadgeText({
+    text: 'OFF'
+  });
+});
+
+const extensions = 'https://developer.chrome.com/docs/extensions';
+const webstore = 'https://developer.chrome.com/docs/webstore';
+
+// When the user clicks on the extension action
+// tab.url.startsWith(extensions) || tab.url.startsWith(webstore)
+chrome.action.onClicked.addListener(async (tab) => {
+  try {
+    if (true) {
+      // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
+      const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+      // Next state will always be the opposite
+      const nextState = prevState === 'ON' ? 'OFF' : 'ON';
+  
+      // Set the action badge to the next state
+      await chrome.action.setBadgeText({
+        tabId: tab.id,
+        text: nextState
+      });
+  
+      if (nextState === 'ON') {
+        // Insert the CSS file when the user turns the extension on
+        await chrome.scripting.insertCSS({
+          files: ['focus-mode.css'],
+          target: { tabId: tab.id }
+        });
+      } else if (nextState === 'OFF') {
+        // Remove the CSS file when the user turns the extension off
+        await chrome.scripting.removeCSS({
+          files: ['focus-mode.css'],
+          target: { tabId: tab.id }
+        });
+      }
+      console.log(tab);
+    }
+  } catch(err) {
+    console.log(err);
+  }
+  
+});
+
+console.log('this is ');
